98c922d3c138777f4185863eaed80b903887a3b8
Yousief Noaman <youseif784@gmail.com>
2022-09-01 16:59:32 +0200

    update project

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: 'http://udagram-env.eba-2vg9b68g.us-east-1.elasticbeanstalk.com/api/v0',
+    apiHost: 'http://Udagram-env.eba-mkjt5maq.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
