febb2005cbbf70934ac19543f8ef0c7f5ae75050
Yousief Noaman <youseif784@gmail.com>
2022-09-02 02:49:35 +0200

    resubmit 2

--- udagram/udagram-frontend/karma.conf.js
+++ udagram/udagram-frontend/karma.conf.js
@@ -2,6 +2,12 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
     config.set({
-        browsers: ['ChromeHeadlessCI'],
+        browsers: ['Chrome'],
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox'],
+            },
+        },
     });
 };
