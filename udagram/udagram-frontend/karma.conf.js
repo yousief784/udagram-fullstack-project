c5abce6328a40c6e7865edfd8d9b4fa3423ae867
Yousief Noaman <youseif784@gmail.com>
2022-09-02 02:43:27 +0200

    resubmit 2

--- udagram/udagram-frontend/karma.conf.js
+++ udagram/udagram-frontend/karma.conf.js
@@ -1,12 +1,7 @@
-// karma.conf.js
-process.env.CHROME_BIN = require('puppeteer').executablePath() // IMPORTANT!
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-module.exports = function(config) {
-  config.set({
-    browsers: ['ChromeHeadless', 'Firefox'], // IMPORTANT! You can list & use multiple browsers
-    plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher') // IMPORTANT!
-    ]
-  })
-}
+module.exports = function (config) {
+    config.set({
+        browsers: ['ChromeHeadlessCI'],
+    });
+};
